1044111c6e17426610197398476c637966d41738
titusIcleanuDev <titus.icleanu.dev@gmail.com>
2022-11-04 21:10:26 +0200

    app.js test comment

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+//git test
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
