a5b659e9290c778847a016f0bbda11a03440a19c
titusIcleanu <titus.icleanu@gmail.com>
2022-11-06 12:09:33 +0200

    538. fixing our seeds fixed seeds file - using cloudinary images fixed /campgronds page to show image on index page

--- seeds/index.js
+++ seeds/index.js
@@ -24,11 +24,20 @@ const seedDb = async () => {
       author: "633f2258886762b8a69a13bd",
       title: `${sample(descriptors)} ${sample(places)}`,
       // image: "http://source.unsplash.com/collection/483251",
-      image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       price: priceRand,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dhlvzthux/image/upload/v1667590777/YelpCamp/ahlk0pibvruzviojy8ng.jpg",
+          filename: "YelpCamp/ahlk0pibvruzviojy8ng",
+        },
+        {
+          url: "https://res.cloudinary.com/dhlvzthux/image/upload/v1667590777/YelpCamp/qg9vnrdcffwagqbpyxfz.jpg",
+          filename: "YelpCamp/qg9vnrdcffwagqbpyxfz",
+        },
+      ],
     });
     await camp.save();
   }
