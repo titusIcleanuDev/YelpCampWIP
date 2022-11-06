f4a81c56474033aa678e86ac02fe72392c11eb26
titusIcleanu <titus.icleanu@gmail.com>
2022-11-06 11:54:24 +0200

    new repo test comment in app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+//06102022 git new repo test
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
