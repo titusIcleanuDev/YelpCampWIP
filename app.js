55ff93c290c12af5e07fce893787a5fb6a4064d6
Titus <titus.icleanu.dev@gmail.com>
2022-11-17 17:42:56 +0200

    github test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+//git test 17.11.2022
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
