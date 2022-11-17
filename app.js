ed5a60cde8f94422da4eae262a1920cfcfeac366
titusIcleanu <titus.icleanu@gmail.com>
2022-11-17 17:53:40 +0200

    test git from titus-laptop

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+//test git from titus-laptop
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
